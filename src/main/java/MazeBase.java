e9b9ca391aac44e3e4caa83938a07defac505c0c
longman555 <realgold555@gmail.com>
2015-09-25 23:33:52 +0900

    mod Pos

--- src/main/java/MazeBase.java
+++ src/main/java/MazeBase.java
@@ -1,9 +1,27 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 class Pos {
+
     public final int x;
     public final int y;
     public Pos(int x, int y) { this.x = x; this.y = y; }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) { return true; }
+        if (!(other instanceof Pos)) { return false; }
+        Pos pos = (Pos)other;
+        return pos.x == this.x && pos.y == this.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + this.x;
+        result = 31 * result + this.y;
+        return result;
+    }
 }
 
 public abstract class MazeBase {
@@ -24,6 +42,7 @@ public abstract class MazeBase {
     protected char[][] maze;
     protected ArrayList<Pos> path;
 
+
     // protected‚ðŽŽ‚µ‚Ä‚Ý‚é
     public MazeBase() {
         maze = new char[YMAX+1][XMAX+1];
@@ -31,12 +50,22 @@ public abstract class MazeBase {
     }
 
     public void showMaze() {
-        for (char[] row : maze) {
-            System.out.println(new String(row));
-        }
+        for (char[] row : maze) { System.out.println(new String(row)); }
     }
 
+    static public ArrayList<Pos> solveMaze(char[][] maze) {
+        LinkedList<Pos> frontier = new LinkedList<>();
+        frontier.add(START_POS);
+        
+        while (!frontier.isEmpty()) {
+            Pos pos = frontier.poll();
+
+        }
+
+        return new ArrayList<Pos>();
+    }
     public ArrayList<Pos> solveMaze() {
+        this.path = solveMaze(maze);
         return path;
     }
 
