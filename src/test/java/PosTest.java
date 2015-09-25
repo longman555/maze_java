6f08c37edc910ba391571604c88399742c1b0806
longman555 <realgold555@gmail.com>
2015-09-26 02:59:49 +0900

    add assertions ofr PosTest

--- src/test/java/PosTest.java
+++ src/test/java/PosTest.java
@@ -3,14 +3,24 @@ import static org.junit.Assert.*;
 
 public class PosTest {
     @Test
-    public void testPosEquals() {
+    public void testEquals() {
         Pos pos = new Pos(0, 0);
         assertEquals(new Pos(0, 0), pos);
+
+        assertEquals(pos, pos);
+
+        pos = new Pos(10, 10);
+        assertNotEquals(new Pos(0, 0), pos);
+
+        assertNotEquals(new Object(), pos);
     }
 
     @Test
     public void testHashCode() {
         Pos pos = new Pos(0, 0);
         assertEquals(new Pos(0, 0).hashCode(), pos.hashCode());
+
+        pos = new Pos(10, 10);
+        assertNotEquals(new Pos(0, 0).hashCode(), pos.hashCode());
     }
 }
