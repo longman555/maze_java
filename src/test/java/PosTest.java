b15ad677319252f6b8d3af04d10e3496e83e1e81
longman555 <realgold555@gmail.com>
2015-09-29 03:36:58 +0900

    add more Pos test

--- src/test/java/PosTest.java
+++ src/test/java/PosTest.java
@@ -23,4 +23,23 @@ public class PosTest {
         pos = new Pos(10, 10);
         assertNotEquals(new Pos(0, 0).hashCode(), pos.hashCode());
     }
+
+    @Test
+    public void testCompareTo() {
+        Pos pos00 = new Pos(0, 0);
+        Pos pos00b = new Pos(0, 0);
+        Pos pos10 = new Pos(1, 0);
+        Pos pos01 = new Pos(0, 1);
+        Pos pos12 = new Pos(1, 2);
+
+        assertTrue(pos00.compareTo(pos00b) == 0);
+        assertTrue(pos00.compareTo(pos10) < 0);
+        assertTrue(pos10.compareTo(pos01) > 0);
+        assertTrue(pos10.compareTo(pos12) > 0);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Pos{0,0}", new Pos(0, 0).toString());
+    }
 }
